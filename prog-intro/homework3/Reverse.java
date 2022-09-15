ac0987abde1b1b80ff6a425111579d830e602d2e
Mond1c <kornilovichmihail@gmail.com>
2022-09-15 21:01:11 +0300

    modified: made Reverse.java faster

--- prog-intro/homework3/Reverse.java
+++ prog-intro/homework3/Reverse.java
@@ -4,25 +4,24 @@ import java.util.Stack;
 public class Reverse {
     public static void main(String[] args) {
         Scanner consoleScanner = new Scanner(System.in);
-        Stack<String> lines = new Stack<>();
+        Stack<Stack<Integer>> lines = new Stack<>();
         while (consoleScanner.hasNextLine()) {
             Scanner stringScanner = new Scanner(consoleScanner.nextLine());
-            StringBuilder stringBuilder = new StringBuilder();
             Stack<Integer> values = new Stack<>();
             while (stringScanner.hasNextInt()) {
                 values.push(stringScanner.nextInt());
             }
-            while (!values.empty()) {
-                stringBuilder.append(values.pop());
-                if (!values.empty()) {
-                    stringBuilder.append(" ");
-                }
-            }
-            stringBuilder.append("\n");
-            lines.push(stringBuilder.toString());
+            lines.push(values);
         }
         while (!lines.empty()) {
-            System.out.print(lines.pop());
+            Stack<Integer> line = lines.pop();
+            while (!line.empty()) {
+                System.out.print(line.pop());
+                if (!line.empty()) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
 }
