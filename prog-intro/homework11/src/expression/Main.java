5a792911dfd027f28bb27c6320e856be1adabeb1
Mond1c <kornilovichmihail@gmail.com>
2022-11-29 09:28:52 +0300

    refacotred

--- prog-intro/homework11/src/expression/Main.java
+++ prog-intro/homework11/src/expression/Main.java
@@ -2,23 +2,22 @@ package expression;
 
 public class Main {
     public static void main(String[] args) {
-        Expression exp = new Subtract(
-                new Multiply(
-                        new Const(2),
-                        new Variable("x")
-                ),
-                new Const(3)
-        );
-        System.out.println(exp.evaluate(5));
-        System.out.println(exp);
-        System.out.println(exp.toMiniString());
-
-        System.out.println(new Multiply(new Const(2), new Variable("x"))
-                .equals(new Multiply(new Const(2), new Variable("x"))));
-
-        System.out.println(new Multiply(new Const(2), new Variable("x"))
-                .equals(new Multiply(new Variable("x"), new Const(2))));
-
-        System.out.println(new Add(new Variable("x"), new Variable("x")).equals(new Add(new Variable("x"), new Variable("x"))));
+        final Expression expression =
+                new Add(
+                        new Subtract(
+                                new Multiply(
+                                        new Variable("x"),
+                                        new Variable("x")
+                                ),
+                                new Multiply(
+                                        new Const(2),
+                                        new Variable("x")
+                                )
+                        ),
+                        new Const(1)
+                );
+        System.out.println(expression);
+        System.out.println(expression.toMiniString());
+        System.out.println(expression.evaluate(1));
     }
 }
