d537e01c4a3011aed074f1ba3dc33c40785ae625
Mikhail Kornilovich <66996619+Mond1c@users.noreply.github.com>
2023-01-13 12:54:13 +0000

    1D done

--- ADS/term1/Lab/1D.cpp
+++ ADS/term1/Lab/1D.cpp
@@ -1,48 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int count = 0;
-
-void merge(vector<int>& a, int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    vector<int> left(n1), right(n2);
-    for (int i = 0; i < n1; i++) {
-        left[i] = a[l + i];
-    }
-    for (int i = 0; i < n2; i++) {
-        right[i] = a[m + 1 + i];
-    }
-    int i = 0, j = 0;
-    int k = l;
-    while (i < n1 && j < n2) {
-        if (left[i] <= right[j]) {
-            arr[k] = left[i];
-            i++;
-            count += ();
+#define ull unsigned long long
+ 
+ull merge(vector<int>& a, vector<int>& b, int l, int h, int m) {
+    int i = l, j = m + 1, k = l;;
+    ull count = 0;
+    while (i <= m && j <= h) {
+        if (a[i] <= a[j]) {
+            b[k++] = a[i++];
         } else {
-            arr[k] = right[j];
-            j++;
+            b[k++] = a[j++];
+            count += (m - i + 1);
         }
-        k++;
     }
-
-    while (i < n1) {
-        arr[k++] = left[i++];
-    }
-
-    while (j < n2) {
-        arr[k++] = right[j++];
-    }
+    while (i <= m) b[k++] = a[i++];
+    for (i = l; i <= h; ++i) a[i] = b[i];
+    return count;
 }
-
-
-void sort(vector<int>& a, int l, int r) {
-    if (l >= r) return;
-    int m = l + (r - l) / 2;
-    sort(a, l, m);
-    sort(a, m + 1, r);
-
-    merge(a, l, m, r);
+ 
+ull sort(vector<int>& a, vector<int>& b, int l, int h) {
+    if (h <= l) {
+        return 0;
+    }
+    int m = (h - l) / 2 + l;
+    ull count = 0;
+    count += sort(a, b, l, m);
+    count += sort(a, b, m + 1, h);
+    count += merge(a, b, l, h, m);
+    return count;
+}
+ 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+    cout << sort(a, b, 0, n - 1) << endl;
+    return 0;
 }
