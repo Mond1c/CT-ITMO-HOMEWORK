eb1fdb9477a12626bd76b8a2333f10489c5efcf8
Mikhail Kornilovich <66996619+Mond1c@users.noreply.github.com>
2023-01-14 08:18:48 +0000

    refactored

--- ADS/term1/Lab/3D.cpp
+++ ADS/term1/Lab/3D.cpp
@@ -1,33 +1,46 @@
 #include <bits/stdc++.h>
 
+struct elem {
+    int v, w, i;
+    double f;
+};
+
+
+bool f(std::vector<elem> &elems, int n, int k, double m) {
+    for (int i = 0; i < n; ++i) {
+        elems[i].f = elems[i].w - elems[i].v / m;
+    }
+    std::sort(elems.begin(), elems.end(), [](const auto& lhs, const auto &rhs) {
+        return lhs.f < rhs.f;
+    });
+    double sum = 0;
+    for (int i = 0; i < k; ++i) {
+        sum += elems[i].f;
+    }
+    return sum >= 0;
+}
+
 int main() {
     int n, k;
     std::cin >> n >> k;
-    double k_best;
-    double v0, w0;
-    std::cin >> v0 >> w0;
-    std::vector<std::pair<int, double>> pairs;
-    pairs.push_back({0, v0 / w0});
-    double prev_v = w0;
-    double prev_w = v0;
-    for (int i = 1; i < n; ++i) {
-        double best = prev_v / prev_w;
-        double v, w;
-        std::cin >> v >> w;
-        double cur_v = prev_v + v;
-        double cur_w = prev_w + w;
-        if (cur_v / cur_w > best) {
-            prev_v = cur_v;
-            prev_w = cur_w;
-            pairs.push_back({i, v / w});
+    std::vector<elem> elems(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> elems[i].v >> elems[i].w;
+        elems[i].i = i + 1;
+    }
+
+    double l = 0, r = 10000001;
+    while (r - l > 0.000000001) {
+        double m = l + (r - l) / 2;
+        if (f(elems, n, k, m)) {
+            l = m;
+        } else {
+            r = m;
         }
     }
-    std::sort(pairs.begin(), pairs.end(), [](const auto& lhs, const auto& rhs) {
-        return lhs.second > rhs.second;
-    });
-    std::cout << pairs.size() << std::endl;
+
     for (int i = 0; i < k; ++i) {
-        std::cout << pairs[i].first << " ";
+        std::cout << elems[i].i << " ";
     }
     std::cout << std::endl;
 }
