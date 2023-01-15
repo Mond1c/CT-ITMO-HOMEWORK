1014704057baf2d7ee6d8f847353ab876cbf498c
Mikhail Kornilovich <66996619+Mond1c@users.noreply.github.com>
2023-01-15 12:27:07 +0000

    added 7A

--- ADS/term1/Lab/1E.cpp
+++ ADS/term1/Lab/1E.cpp
@@ -1,25 +1,80 @@
 #include <bits/stdc++.h>
-#define ull unsigned long long
 
-ull solve(std::vector<int> &a, int i, int k, ull count) {
-    if (i == a.size()) {
-        if (k <= 0) {
-            count++;
+struct Player {
+    int clan = -1;
+    int exp=0;
+};
+
+std::unordered_map<int, std::vector<std::shared_ptr<Player>>> clans;
+std::unordered_map<int, std::shared_ptr<Player>> players;
+int clans_count = 0;
+
+void add(int x, int v) {
+    if (!players.count(x)) {
+        players[x] = std::make_shared<Player>();
+    } 
+    if (players[x]->clan != -1) {
+        for (const auto& player : clans[players[x]->clan]) {
+            player->exp += v;
         }
-        return count;
+    } else {
+        players[x]->exp += v;
     }
-    count = solve(a, i + 1, k - a[i], count);
-    count = solve(a, i + 1, k, count);
-    return count;
+}
+
+void join(int x, int y) {
+    std::vector<std::shared_ptr<Player>> new_clan;
+    if (!players.count(x)) {
+        players[x] = std::make_shared<Player>();
+    }
+    if (!players.count(y)) {
+        players[y] = std::make_shared<Player>();
+    }
+    if (players[x]->clan != -1) {
+        for (const auto& player : clans[players[x]->clan]) {
+            new_clan.push_back(player);
+        }
+        clans.erase(players[x]->clan);
+    } else {
+        new_clan.push_back(players[x]);
+    }
+    if (players[y]->clan != -1) {
+        for (const auto& player : clans[players[y]->clan]) {
+            new_clan.push_back(player);
+        }
+        clans.erase(players[y]->clan);
+    } else {
+        new_clan.push_back(players[y]);
+    }
+    clans[clans_count] = new_clan;
+    players[y]->clan = players[x]->clan = clans_count++;
+}
+
+int get(int x) {
+    if (!players.count(x)) {
+        return 0;
+    }
+    return players[x]->exp;
 }
 
 int main() {
-    int n, k;
-    std::cin >> n >> k;
-    std::vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        std::cin >> a[i];
+    int n, m;
+    std::cin >> m >> n;
+    for (int i = 0; i < n; ++i) {
+        std::string request;
+        std::cin >> request;
+        if (request == "add") {
+            int x, v;
+            std::cin >> x >> v;
+            add(x, v);
+        } else if (request == "join") {
+            int x, y;
+            std::cin >> x >> y;
+            join(x, y);
+        } else {
+            int x;
+            std::cin >> x;
+            std::cout << get(x) << std::endl;
+        }
     }
-    std::cout << solve(a, 0, k, 0) << std::endl;
-    return 0;
 }
