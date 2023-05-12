606e358d7c70fde86f9bda0abe36946a0a9fba91
Mikhail Kornilovich <kornilovichmihail@gmail.com>
2023-05-12 12:04:58 +0300

    Added task C and E to dfa lab

--- dm/term2/Lab/DFA/C.cpp
+++ dm/term2/Lab/DFA/C.cpp
@@ -1,40 +1,77 @@
 #include <bits/stdc++.h>
 
-const std::string OUTPUT_FILE = "problem2.out";
-
-void write_answer(const std::string &s) {
+const std::string OUTPUT_FILE = "problem3.out";
+  
+void write_answer(const std::string& s) {
     std::ofstream out(OUTPUT_FILE);
     out.write(s.c_str(), s.length());
     out.close();
 }
 
-
-struct hash {
-    std::size_t operator()(const std::pair<int, char> &key) const {
-        return std::hash<int>{}(key.first) + std::hash<char>{}(key.second);
+void dfs(int v, std::vector<std::vector<int>>& dfa, std::vector<int>& d) {
+    if (d[v] > 0) {
+        return;
     }
-};
+    d[v] = 1;
+    for (int i : dfa[v]) {
+        dfs(i, dfa, d);
+    }
+}
+
+bool inf(int v, std::vector<std::vector<int>>& dfa, std::vector<int>& d) {
+    if (d[v] == 0) return false;
+    if (d[v] == 2) return true;
+    d[v] = 2;
+    for (int i : dfa[v]) {
+        if (inf(i, dfa, d)) {
+            d[v] = 1;
+            return true;
+        }
+    } 
+    d[v] = 1;
+    return false;
+}
+
+int count(int v, std::vector<std::vector<int>>& dfa, std::set<int>& end, std::vector<int>& d) {
+    if (d[v] == 0) return 0;
+    int res = 0;
+    if (end.find(v) != end.end()) {
+        ++res;
+    }
+    for (int i : dfa[v]) {
+        res = (res + count(i, dfa, end, d)) % 1'000'000'007;
+    }
+    return res;
+}
 
 int main() {
-    std::ifstream in("problem2.in");
-    std::string str;
-    in >> str;
     int n, m, k;
+    std::ifstream in("problem3.in");
     in >> n >> m >> k;
-    std::vector<bool> end(n + 1);
-    std::unordered_map<std::pair<int, char>, std::unordered_set<int>, hash> graph;
+    std::set<int> end;
     for (int i = 0; i < k; i++) {
-        int a;
-        in >> a;
-        end[a] = true;
+        int value;
+        in >> value;
+        end.insert(value - 1);
     }
+    std::vector<std::vector<int>> dfa(n);
+    std::vector<std::vector<int>> dfa_rev(n);
     for (int i = 0; i < m; i++) {
         int a, b;
+        in >> a >> b;
         char c;
-        in >> a >> b >> c;
-        graph[{a, c}].insert(b);
+        in >> c;
+        dfa[a - 1].push_back(b - 1);
+        dfa_rev[b - 1].push_back(a - 1);
     }
-    in.close();
-
+    std::vector<int> d(n);
+    for (int i : end) {
+        dfs(i, dfa_rev, d);
+    }
+    if (inf(0, dfa, d)) {
+        write_answer("-1");
+    } else {
+        write_answer(std::to_string(count(0, dfa, end, d)));
+    }
     return 0;
 }
--- dm/term2/Lab/DFA/E.cpp
+++ dm/term2/Lab/DFA/E.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
-
+ 
 const int MOD = 1'000'000'007;
-
+ 
 template<class T>
 struct matrix {
 public:
     matrix(std::size_t n, std::size_t m) : data_(std::vector<std::vector<T>>(n, std::vector<T>(m))),
                                            n(n), m(m) {}
-
+ 
 public:
     friend matrix operator*(const matrix &left, const matrix &right) {
         matrix result(left.n, right.m);
@@ -15,13 +15,23 @@ public:
             for (int j = 0; j < right.m; j++) {
                 result.data_[i][j] = T();
                 for (int k = 0; k < left.m; k++) {
-                    result.data_[i][j] = (result.data_[i][j] + (left.data_[i][k] * right.data_[k][j]) % MOD) % MOD;
+                    result.data_[i][j] = (result.data_[i][j] + (left.data_[i][k] * right.data_[k][j])) % MOD;
                 }
             }
         }
         return result;
     }
 
+    friend std::ostream& operator<<(std::ostream& out, const matrix& mat) {
+        for (int i = 0; i < mat.n; i++) {
+            for (int j = 0; j < mat.m; j++) {
+                out << mat[i][j] << " ";
+            }
+            out << std::endl;
+        }
+        return out;
+    }
+ 
     static matrix pow(matrix m, int power) {
         if (power == 1) {
             return m;
@@ -31,11 +41,15 @@ public:
         }
         return pow(m, power - 1) * m;
     }
-
+ 
     std::vector<T> &operator[](size_t index) {
         return data_[index];
     }
-
+    
+    std::vector<T> operator[](size_t index) const {
+        return data_[index];
+    }
+ 
 private:
     std::vector<std::vector<T>> data_;
     std::size_t n;
@@ -43,97 +57,94 @@ private:
 };
 
 struct hash {
-    std::size_t operator()(const std::pair<int, char> &key) const {
+    size_t operator()(const std::pair<int, char>& key) const {
         return std::hash<int>{}(key.first) + std::hash<char>{}(key.second);
     }
 };
 
-struct stHash {
-    std::size_t operator()(const std::unordered_set<int> &key) const {
-        std::size_t ans = 0;
-        for (int i : key) {
-            ans += std::hash<int>{}(i);
-        }
-        return ans;
-    }
-};
-
-void convert_to_dfa(int s, std::unordered_set<char>& alp, std::vector<int> &end,
-                    std::unordered_map<std::pair<int, char>, std::unordered_set<int>, hash> &graph, int& n) {
-    std::queue<std::unordered_set<int>> P;
-    P.push({s});
-    std::unordered_set<std::unordered_set<int>, stHash> Q;
-    std::unordered_map<std::pair<int, char>, std::unordered_set<int>, hash> ans_graph;
-    while (!P.empty()) {
-        auto pd = P.front();
-        P.pop();
-        for (char c : alp) {
-            std::unordered_set<int> q;
-            for (int p : pd) {
-                q.insert(graph[{p, c}].begin(), graph[{p, c}].end());
-            }
-            for (int p : pd) {
-                ans_graph[{p, c}] = q;
-            }
-            if (!Q.count(q)) {
-                P.push(q);
-                Q.insert(q);
-            }
-        }
-    }
-    std::unordered_set<int> ans_end;
-    std::unordered_set<int> count;
-    for (const auto& q : Q) {
-        for (int p : end) {
-            if (q.count(p)) {
-                for (int qd : q) {
-                    ans_end.insert(qd);
+std::map<std::pair<int, char>, int> convert(std::map<std::pair<int, char>, std::set<int>>& nfa, std::set<char> chars,
+std::vector<int>& ends, int& n) {
+    std::map<std::pair<int, char>, int> dfa;
+    std::queue<std::set<int>> states;
+    std::vector<int> new_ends;
+    std::map<std::set<int>, int> mp;
+    std::set<std::set<int>> visisted;
+    int i = 0;
+    states.push({1});
+    while (!states.empty()) {
+        auto cur = states.front(); states.pop();
+        for (char ch : chars) {
+            std::set<int> s;
+            bool isEnd = false;
+            for (int state : cur) {
+                if (nfa.find({state, ch}) != nfa.end()) {
+                    for (int p : nfa[{state, ch}]) {
+                        s.insert(p);
+                        if (std::find(ends.begin(), ends.end(), p) != ends.end()) {
+                            isEnd = true;
+                        }
+                    }
                 }
-                break;
+            }
+            if (!s.empty()) {
+                if (mp.find(cur) == mp.end()) {
+                    mp[cur] = i++;
+                }
+                if (mp.find(s) == mp.end()) {
+                    mp[s] = i++;
+                }
+                int j = mp[cur];
+                dfa[{j, ch}] = mp[s];
+                if (isEnd) {
+                    new_ends.push_back(mp[s]);
+                }
+                if (visisted.find(s) == visisted.end()) {
+                    visisted.insert(s);
+                    states.push(s);
+                }
             }
         }
-        for (int qd : q) {
-            count.insert(qd);
-        }
     }
-    ans_graph;
-    end = std::vector<int>(ans_end.begin(), ans_end.end());
-    n = count.size();
+    n = i;
+    ends = new_ends;
+    return dfa;
 }
-
 
 int main() {
     int n, m, k, l;
     std::ifstream in("problem5.in");
     in >> n >> m >> k >> l;
     std::vector<int> end(k);
-    std::unordered_map<std::pair<int, char>, std::unordered_set<int>, hash> graph;
-    std::unordered_set<char> alp;
     for (int i = 0; i < k; i++) {
         int value;
         in >> value;
-        end[i] = value - 1;
+        end[i] = value;
     }
-    // matrix<long long> mat(n, n);
+    std::map<std::pair<int, char>, std::set<int>> graph;
+    std::set<char> chars;
     for (int i = 0; i < m; i++) {
         int a, b;
         char ch;
         in >> a >> b >> ch;
-        alp.insert(ch);
-        graph[{a - 1, ch}].insert(b - 1);
+        chars.insert(ch);
+        graph[{a, ch}].insert(b);
     }
     in.close();
-    convert_to_dfa(0, alp, end, graph, n);
+    std::map<std::pair<int, char>, int> dfa = convert(graph, chars, end, n);
+    //std::cout << dfa.size() << std::endl;
     matrix<long long> mat(n, n);
-    for (const auto& p : graph) {
-        if (!p.second.empty()) {
-            mat[p.first.first - 1][*p.second.begin() - 1]++;
-        }
+    for (const auto& item : dfa) {
+        //std::cout << item.first.first - 1 << " " << item.second - 1 << std::endl;
+        mat[item.first.first][item.second]++;
     }
+    //std::cout << mat << std::endl;
     mat = matrix<long long>::pow(mat, l);
     long long result = 0;
-    for (int e: end) {
-        result = (result + mat[0][e - 1]) % MOD;
+    //std::cout << mat << std::endl;
+    std::set<int> ends(end.begin(), end.end());
+    for (int e : ends) {
+     //   std::cout << e << std::endl;
+        result = (result + mat[0][e]) % MOD;
     }
     std::ofstream out("problem5.out");
     out << result;
